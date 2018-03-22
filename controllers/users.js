3f0b2ae3ba7c3e5de5bd630b2e51a13a7d02641f
Allyson Finck <allyson.finck@gmail.com>
2018-03-22 15:26:24 -0400

    functional create user and login

--- controllers/users.js
+++ controllers/users.js
@@ -7,4 +7,11 @@ router.get('/new', (req, res)=>{
   res.render('users/new.ejs')
 })
 
+router.post('/', (req, res)=>{
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+  User.create(req.body, (err, createdUser)=>{
+    res.redirect('/');
+  })
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 const port = process.env.PORT || 3000;
 
+
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(session({
+  secret: 'feedmeseymour',
+  resave: false,
+  saveUninitialized: false
+}))
+
 
 const hikesController = require('./controllers/hikeposts.js');
 app.use('/hikes', hikesController);
@@ -16,15 +23,22 @@ app.use('/hikes', hikesController);
 const usersController = require('./controllers/users.js');
 app.use('/users', usersController);
 
+const sessionsController = require('./controllers/sessions.js');
+app.use('/sessions', sessionsController);
+
 app.get('/', (req, res)=>{
-  res.render('index.ejs');
+  res.render('index.ejs', {
+    currentUser: req.session.currentUser
+  });
 })
+
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/alpinebeta';
 mongoose.connect(mongoURI);
 mongoose.connection.once('open', ()=>{
   console.log('connected to mongo');
 })
+
 
 app.listen(port, ()=>{
   console.log('listening');
